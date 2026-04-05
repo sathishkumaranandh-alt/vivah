9d3d80aa290e1f4dff368649c0ff1f9f0780eb08
sathishkumaranandh-alt <sathishkumaranandh@gmail.com>
2026-04-05 15:09:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ const authLimiter = rateLimit({
 // ──────────────────────────────────────────────────────────────
 //  DATABASE CONNECTION
 // ──────────────────────────────────────────────────────────────
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URL || process.env.MONGO_URI,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
