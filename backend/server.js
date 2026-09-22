4899e06a11147bc5435942b2d89954e527a87f88
sathishkumaranandh-alt <sathishkumaranandh@gmail.com>
2026-09-22 21:53:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,4 +36,4 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`)); 
